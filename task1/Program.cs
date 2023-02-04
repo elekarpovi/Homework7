bb0aae6f451ec539c75f776cf84ee5ed9371f452
Elena Karpovich <elekarpovi@tut.by>
2023-02-04 20:32:21 +0300

    Added string format

--- task1/Program.cs
+++ task1/Program.cs
@@ -34,7 +34,7 @@ for (int i = 0; i < array.GetLength(0); i++)
     {
         for (int j = 0; j < array.GetLength(1); j++)
         {
-            System.Console.Write(array[i, j] + " ");         
+            System.Console.Write(String.Format("{0,9}", array[i, j]));         
         }
         System.Console.WriteLine();
         
